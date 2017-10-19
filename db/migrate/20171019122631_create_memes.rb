26925c1d3c7fa88a1bc21a80ce638f8bd216e6f2
Elise & Kristin MacDonough & Toby Ladislas & Patrick Rodriguez <pair+empoplawski.kmac02.majesty86.patrodriguez108@devbootcamp.com>
2017-10-19 14:58:23 -0500

    Generate and create migrations

--- db/migrate/20171019122631_create_memes.rb
+++ db/migrate/20171019122631_create_memes.rb
@@ -1,4 +1,10 @@
 class CreateMemes < ActiveRecord::Migration[5.1]
   def change
+    create_table :memes do |t|
+      t.string :photo_path, null: false
+      t.integer :memer_id, null: false
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20171019122638_create_captions.rb
+++ db/migrate/20171019122638_create_captions.rb
@@ -1,4 +1,11 @@
 class CreateCaptions < ActiveRecord::Migration[5.1]
   def change
+    create_table :captions do |t|
+      t.string :caption_content, null: false
+      t.integer :meme_id, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
   end
 end
