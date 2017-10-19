fc399e827853a7e10d6c133c6ec6460dca83b56a
Elise & Kristin MacDonough & Toby Ladislas & Patrick Rodriguez <pair+empoplawski.kmac02.majesty86.patrodriguez108@devbootcamp.com>
2017-10-19 15:07:39 -0500

    Remove colon. Ouch.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :commenter, class_name: "User"
-  belongs_to :meme, polymorphic: :true
-  belongs_to :caption, polymorphic: :true
+  belongs_to :meme, polymorphic: true
+  belongs_to :caption, polymorphic: true
 end
