1b1cbf7c0ae819b08ae529e08a16a81ee86e681d
Elise & Kristin MacDonough <pair+empoplawski.kmac02@devbootcamp.com>
2017-10-21 17:55:35 -0500

    Add event handlers for voting

--- app/controllers/memes.rb
+++ app/controllers/memes.rb
@@ -11,10 +11,15 @@ end
 
 post '/memes/:id/upvote' do
   @meme = Meme.find(params[:id])
-  @meme.votes.create(voter_id: current_user.id, value: 1)
+  new_vote = @meme.votes.new(voter_id: current_user.id, value: 1)
+  vote_count = @meme.total_votes
+  if !new_vote.save
+    new_vote.delete
+    vote_count -= 1
+  end
   if request.xhr?
       content_type :json
-      {vote_count: vote.value}
+      {vote_count: vote_count}.to_json
   else
     redirect "/memes/#{@meme.id}"
   end
@@ -22,11 +27,15 @@ end
 
 post '/memes/:id/downvote' do
   @meme = Meme.find(params[:id])
-  @meme.votes.create(voter_id: current_user.id, value: -1)
-
+  new_vote = @meme.votes.new(voter_id: current_user.id, value: -1)
+  vote_count = @meme.total_votes
+  if !new_vote.save
+    new_vote.delete
+    vote_count -= 1
+  end
   if request.xhr?
       content_type :json
-      {vote_count: vote.value}
+      {vote_count: vote_count}.to_json
   else
     redirect "/memes/#{@meme.id}"
   end
--- public/js/application.js
+++ public/js/application.js
@@ -26,33 +26,11 @@ $(document).ready(function() {
       data: $form.serialize()
     });
 
-
-
-
     promise.done(function(response) {
       $(".signup").remove();
       $(".login").replaceWith(response);
       $form.remove();
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     promise.fail(function(response) {
       var errors = JSON.parse(response.responseText);
@@ -60,12 +38,76 @@ $(document).ready(function() {
       errors.forEach(function(error) {
         $(".errors").children(".messages").append(`<li>${error}</li>`)
       })
-
     })
-
-
   })
 
+  $("#container").on("submit", ".vote-form-upvote-meme", function(event) {
+    event.preventDefault();
+    var form = $(this)
+    var url = form.attr("action");
+    var method = form.attr("method");
+
+    var request = $.ajax({
+      method: method,
+      url: url
+    });
+
+    request.done(function(vote){
+      var meme_total_votes = form.siblings(".vote_count")
+      meme_total_votes.text(vote["vote_count"])
+    });
+  });
+
+  $("#container").on("submit", ".vote-form-downvote-meme", function(event) {
+    event.preventDefault();
+    var form = $(this)
+    var url = form.attr("action");
+    var method = form.attr("method");
+
+    var request = $.ajax({
+      method: method,
+      url: url
+    });
+
+    request.done(function(vote){
+      var meme_total_votes = form.siblings(".vote_count")
+      meme_total_votes.text(vote["vote_count"])
+    });
+  });
+
+  $(".caption-ul-list").on("submit", ".vote-form-upvote-caption", function(event) {
+    event.preventDefault();
+    var form = $(this)
+    var url = form.attr("action");
+    var method = form.attr("method");
+
+    var request = $.ajax({
+      method: method,
+      url: url
+    });
+
+    request.done(function(vote){
+      var caption_total_votes = form.siblings(".vote_count")
+      caption_total_votes.text(vote["vote_count"])
+    });
+  });
+
+   $(".caption-ul-list").on("submit", ".vote-form-downvote-caption", function(event) {
+    event.preventDefault();
+    var form = $(this)
+    var url = form.attr("action");
+    var method = form.attr("method");
+
+    var request = $.ajax({
+      method: method,
+      url: url
+    });
+
+    request.done(function(vote){
+      var caption_total_votes = form.siblings(".vote_count")
+      caption_total_votes.text(vote["vote_count"])
+    });
+  });
 
 
 });
